b6371fa6bfe5c89626b577332767afa4fa22cd9d
kim-money <wangingimoses102@gmail.com>
2022-12-13 08:12:58 -0800

    correction 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - prints _putchar and a new line
+ * _putchar and a new line
  * Return: (0)
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
