6efaee48551f88241fc58bc36b48ebeaec070110
kim-money <wangingimoses102@gmail.com>
2022-12-13 10:52:42 -0800

    try 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
